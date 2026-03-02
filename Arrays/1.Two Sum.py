ceb35caca8da59c338743a2188991b750dcdcd8c
Hrutuja <hrutujaumratkar123@gmail.com>
2026-03-02 22:59:22 +0530

    Initial commit

---
+++ Arrays/1.Two Sum.py
@@ -0,0 +1,13 @@
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+# You can return the answer in any order.
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        hash_map= {}
+
+        for i,num in enumerate(nums):
+            if target-num in hash_map:
+                return [hash_map[target-num], i]
+            hash_map[num] = i
+        return []
